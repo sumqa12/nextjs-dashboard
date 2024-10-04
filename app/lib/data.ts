541237b58915080fcc08ac5c4e0b13d815bc082a
sumqa12 <atsuucstgw1214@gmail.com>
2024-10-04 12:23:37 +0900

    チャプター13まで

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -141,7 +141,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
-  try {
+	try {
     const data = await sql<InvoiceForm>`
       SELECT
         invoices.id,
@@ -158,6 +158,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+		console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
